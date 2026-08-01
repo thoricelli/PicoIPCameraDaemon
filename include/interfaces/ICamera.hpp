ee69119b3ed4323a2493ab1017caa219948a3dd1
thoricelli <29801268+thoricelli@users.noreply.github.com>
2026-08-01 17:04:28 +0200

    fix: add ICamera interface for generic camera classes.

---
+++ include/interfaces/ICamera.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+class ICamera
+{
+public:
+    /**
+     * Opens this camera.
+     * @returns If the camera opened succesfully.
+     */
+    virtual bool open() = 0;
+    /**
+     * Closes and cleans up this camera.
+     * @returns If the camera has closed succesfully.
+     */
+    virtual bool close() = 0;
+
+    /**
+     * @returns If the camera is currently open.
+     * @note Implementation note: Must be thread safe!
+     */
+    virtual bool isOpen() = 0;
+};
